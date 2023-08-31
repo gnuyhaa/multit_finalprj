0b6d96dbb3afe694afcc37931074ac912484fbc7
A Hyung Kim <125522834+gnuyhaa@users.noreply.github.com>
2023-08-31 17:06:06 +0900

    Update settings.py

--- 장고_최종/core/settings.py
+++ 장고_최종/core/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-mdxl(@jb9u&11q(8t-zhv1v@znxs%6uet34p#__t6sbtw+&pzy"
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
